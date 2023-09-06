13dc3e5dd03b49cccae5d8c27bc2a43ee34a7260
inge-perez <inge.jsperez@gmail.com>
2023-09-06 17:58:10 -0500

    mi primer commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handle');
+
+const app = express();
+const port = 3010;
+
+app.use(express.json());
+const whitelist = ['http://localhost:8080'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en Express');
+});
+
+routerApi(app);
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log('My port: ' + port);
+});
